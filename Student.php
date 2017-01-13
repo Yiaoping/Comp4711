dab6013d760a9dd6f64453b4593de70c627ce31f
Yiaoping <yiao2873@hotmail.com>
2017-01-12 20:43:28 -0800

    Added comments to student and index files

--- Student.php
+++ Student.php
@@ -7,12 +7,16 @@
  */
 
 /**
- * Description of Student
+ * Description of Student. This class contains functions to construct a student.
  *
  * @author Yiaoping
  */
 class Student {
     
+    /*
+     * This function is used to construct the student indicating their first and
+     * last name, their email, and their grades.
+     */
     function _construct(){
         $this->surname =  '';
         $this->first_name = '';
@@ -20,27 +24,43 @@ class Student {
         $this->grades = array();
     }
     
+    /*
+     * This function is used to add an email for the student.
+     */
     function add_email($which, $address){
         $this->emails[$which]=$address;
     }
     
+    /*
+     * This function is used to add a grade to the student.
+     */
     function add_grade($grade){
         $this->grades[] = $grade;
     }
     
+    /*
+     * This function is used to find the average score of the student's marks.
+     */
     function average(){
         $total = 0;
-        foreach ($this->grades as $value)
+        foreach ($this->grades as $value){
             $total += $value;
+        }
         return $total/ count($this->grades);
     }
     
-    function toString(){
-        $result = $this->first_name.' '.$this->surname;
-        $result .= ' ('.$this->average().")\n";
-        foreach($this->emails as $which=>$what)
-            $result .= $which . ': '.$what."\n";
-        $result .="\n";
-        return '<pre>'.$result.'<\pre>';
+    /*
+     * This function is used to format and output the information of a student
+     * nicely.
+     */
+    function toString() {
+    $result = $this->first_name . ' ' . $this->surname;
+    $result .= ' ('.$this->average().")\n";
+    foreach($this->emails as $which=>$what){
+        $result .= $which . ': '. $what. "\n";
     }
+    $result .= "\n";
+    return '<pre>'.$result.'</pre>';
+    }
+
 }
