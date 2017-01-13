477b58f10a76c7ad3cd7d1efe263f77041e56197
Yiaoping <yiao2873@hotmail.com>
2017-01-12 20:02:17 -0800

    Added constructor, add email and add grade function.

--- Student.php
+++ Student.php
@@ -12,5 +12,19 @@
  * @author Yiaoping
  */
 class Student {
-    //put your code here
+    
+    function _construct(){
+        $this->surname =  '';
+        $this->first_name = '';
+        $this->emails = array();
+        $this->grades = array();
+    }
+    
+    function add_email($which, $address){
+        $this->emails[$which]=$address;
+    }
+    
+    function add_grade($grade){
+        $this->grades[] = $grade;
+    }
 }
